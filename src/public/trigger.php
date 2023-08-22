4e42a85b233d1efa8d05fb4a715c9701fa3f6f26
EncryptEx <limusina10@gmail.com>
2023-08-22 18:14:31 +0200

    Replaced action type detection to ids

--- src/public/trigger.php
+++ src/public/trigger.php
@@ -43,99 +43,63 @@ if ($doesExist['success']) {
     if (isset($_FILES) && count($_FILES) == 1) {
 
         $uploaddir = "/../private/uploads/";
-        $newName = "log" . time() . "-". $triggerId . ".txt";
+        $newName = "log" . time() . "-" . $triggerId . ".txt";
         $uploadfile = __DIR__ . $uploaddir . $newName;
 
 
         $allowed = array('txt', 'log');
         $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
         if (!in_array($ext, $allowed)) {
-            http_response_code(500);
-            print(json_encode(
-                [
-                    'success' => false,
-                    'message' => 'Only .txt and .log extensions are supported',
-                    'timestamp' => time()
-                ]
-            ));
-            die();
+            $utils->showError('Only .txt and .log extensions are supported');
         }
 
         if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
             $filePath = $uploadfile;
         } else {
-            http_response_code(500);
-            print(json_encode(
-                [
-                    'success' => false,
-                    'message' => 'There was an error while uploading the file',
-                    'timestamp' => time()
-                ]
-            ));
-            die();
+            $utils->showError('There was an error while uploading the file');
         }
     }
 
 
-    if ($utils->getActionName($actionToDo) == "email") {
+    if ($utils->getActionId($actionToDo) == 1) { // is email (alert)
         # send email to ownerId
 
         $emailResult = $utils->sendEmailTo($ownerId, $triggerName, $stringUrl, $data, $status, $filePath);
 
         if (!$emailResult['success']) {
-            http_response_code(500);
-            print(json_encode(
-                [
-                    'success' => false,
-                    'message' => $emailResult['message'],
-                    'timestamp' => time()
-                ]
-            ));
-            die();
+            $utils->showError($emailResult['message']);
         }
     } else {
         # id does not match, throw error
-        http_response_code(500);
-        print(json_encode(
-            [
-                'success' => false,
-                'message' => 'There was an error while trying to fetch the action type',
-                'timestamp' => time()
-            ]
-        ));
-        die();
+        $utils->showError('There was an error while trying to fetch the action type');
     }
 
     # save log
     $result = $utils->triggerLog($triggerId, $data, $newName);
 
-    if (!$result) {  # if couldn't be saved, throw error
-        http_response_code(500);
-        print(json_encode(
-            [
-                'success' => false,
-                'message' => 'There was an error while trying to save the log',
-                'timestamp' => time()
-            ]
-        ));
-        die();
+    if (!$result) { # if couldn't be saved, throw error
+        $utils->showError('There was an error while trying to save the log');
     }
 
     http_response_code(200);
-    print(json_encode(
-        [
-            'success' => true,
-            'message' => 'Action performed successfully!',
-            'timestamp' => time()
-        ]
-    ));
+    print(
+        json_encode(
+            [
+                'success' => true,
+                'message' => 'Action performed successfully!',
+                'timestamp' => time()
+            ]
+        )
+    );
 } else {
     http_response_code(404);
-    print(json_encode(
-        [
-            'success' => false,
-            'message' => 'The requested TriggerID does not exist',
-            'timestamp' => time()
-        ]
-    ));
+    print(
+        json_encode(
+            [
+                'success' => false,
+                'message' => 'The requested TriggerID does not exist',
+                'timestamp' => time()
+            ]
+        )
+    );
 }
